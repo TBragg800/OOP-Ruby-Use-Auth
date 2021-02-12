26b3e7fa370cf4a0eec4bf4b7566b4ddd1fb8b36
LM&T <tbbragg83@gmail.com>
2021-02-12 02:28:24 -0600

    created auth module

--- learn_bcrypt.rb
+++ learn_bcrypt.rb
@@ -1,1 +1,45 @@
 
+# users = [
+  #             {username: "Thomas", password: "password1"},
+  #             {username: "Maria", password: "password2"},
+  #             {username: "Little", password: "password3"},
+  #             {username: "Cyrus", password: "password4"},
+  # ]
+
+module Crud
+
+  require 'bcrypt'
+
+  p "module crud activated"
+    
+  def self.create_hash_digest(password)
+    BCrypt::Password.create(password)
+  end
+
+  def self.verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+
+  def self.create_secure_users(list_of_users)
+    list_of_users.each do |user_record|
+      user_record[:password] = create_hash_digest(user_record[:password])
+    end
+    list_of_users
+  end
+
+  # new_users = create_secure_users(users)
+  # puts new_users
+
+  def self.authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+        return user_record
+      end
+    end
+    "Credentials were not found"
+  end
+
+end
+
+# puts authenticate_user("Little", "password3", new_users)
+# puts authenticate_user("Cyrus", "password4", new_users)
